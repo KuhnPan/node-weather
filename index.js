d016df6e75c6f535ea7e385fc40deeb6eece2538
Kuhn Pan <kuhn.pan@gmail.com>
2018-03-28 00:26:18 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let request = require('request');
+
+let apiKey = '06fca7b66b0842d27f5f977c71eb81e3';
+let city = 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    console.log('body:', body);
+  }
+});
