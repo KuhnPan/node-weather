c70d419aafbbee0d76e3f4a5da35eeec1176df3a
Kuhn Pan <kuhn.pan@gmail.com>
2018-03-28 01:24:12 +0800

    add yargs module

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-let request = require('request');
+const request = require('request');
+const argv = require('yargs').argv;
 
 let apiKey = '06fca7b66b0842d27f5f977c71eb81e3';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let city = argv.c || 'shanghai';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
